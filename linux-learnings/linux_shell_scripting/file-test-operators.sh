9f4ff7e573b475c4e0906adb23f8073d1e7853d7
berkay <karatay.berkay@gmail.com>
2023-12-24 14:56:46 +0300

    linux .sh updated

--- linux-learnings/linux_shell_scripting/file-test-operators.sh
+++ linux-learnings/linux_shell_scripting/file-test-operators.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 # this is a comment
+
+echo -e "Enter the name of file : \c"
+read file_name
+
+# A file exits or not
+if [ -e $file_name ]
+then
+  echo "$file_name is found"
+else
+  echo "$file_name is not found"
+fi
+
+# Is it a file or not
+if [ -f $file_name ]
+then
+  echo "$file_name is found"
+else
+  echo "$file_name is not found"
+fi
+
+# Check Directory
+if [ -f $file_name ]
+then
+  echo "$file_name is found"
+else
+  echo "$file_name is not found"
+fi
